867dd3a474942205fb0df6dcec349621ce2be261
Jozef Izso <jozef.izso@gmail.com>
2020-06-29 21:38:09 +0200

    Create sample PowerPoint automation app

---
+++ samples/PowerPointAutomation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace NetOfficeSamples.PowerPointAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Application();
+            var presentation = app.Presentations.Add(MsoTriState.msoTrue);
+            var slide = presentation.Slides.Add(1, PpSlideLayout.ppLayoutText);
+
+            if (slide.Shapes.Count > 1)
+            {
+                var shape = slide.Shapes[1];
+                shape.TextFrame.TextRange.Text = "Welcome to PowerPoint";
+            }
+
+            app.Quit();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
