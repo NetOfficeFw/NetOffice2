103ee0ade6eb8bd7a4f5281d66481b55f783dca9
Jozef Izso <jozef.izso@gmail.com>
2020-07-08 21:24:46 +0200

    Implement sample addin for PowerPoint

---
+++ samples/PowerPointAddin/PowerPointAddin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using NetOffice.Office;
+
+namespace NetOfficeSamples
+{
+    [ComVisible(true)]
+    [Guid("D8494D38-D995-4670-AFF9-9425ED71D657")]
+    [ProgId("NetOfficeSamples.PowerPointAddin")]
+    public class PowerPointAddin : IDTExtensibility2
+    {
+        public void OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+        {
+            Trace.WriteLine($"Addin connected to application. Mode: {connectMode}");
+            var type = application.GetType();
+            var name = type.FullName;
+            var isCom = Marshal.IsComObject(application);
+
+            try
+            {
+                var unknown = Marshal.GetIUnknownForObject(application);
+                //var dispatch = Marshal.GetIDispatchForObject(application);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Marshal failed. {ex}");
+            }
+        }
+
+        public void OnDisconnection([In] ext_DisconnectMode removeMode, [In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+            Trace.WriteLine($"Addin disconnecting from application. Mode: {removeMode}");
+        }
+
+        public void OnAddInsUpdate([In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+        }
+
+        public void OnStartupComplete([In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+            Trace.WriteLine($"Addin startup completed.");
+        }
+
+        public void OnBeginShutdown([In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+        }
+    }
+}
---
+++ samples/PowerPointAddin/register-addin.cmd
@@ -0,0 +1,10 @@
+C:\Windows\SysWOW64\regsvr32.exe /s PowerPointAddin.comhost.dll
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\PowerPoint\Addins\NetOfficeSamples.PowerPointAddin" /f /v LoadBehavior /t REG_DWORD /d 3
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\PowerPoint\Addins\NetOfficeSamples.PowerPointAddin" /f /v FriendlyName /t REG_SZ /d "PowerPoint Addin (.NET Core 3.1 / NetOffice 2)"
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\PowerPoint\Addins\NetOfficeSamples.PowerPointAddin" /f /v Description /t REG_SZ /d "Sample addin running in .NET Core 3.1 using NetOffice 2 alpha"
+
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\Word\Addins\NetOfficeSamples.PowerPointAddin" /f /v LoadBehavior /t REG_DWORD /d 3
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\Word\Addins\NetOfficeSamples.PowerPointAddin" /f /v FriendlyName /t REG_SZ /d "PowerPoint Addin (.NET Core 3.1 / NetOffice 2)"
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\Word\Addins\NetOfficeSamples.PowerPointAddin" /f /v Description /t REG_SZ /d "Sample addin running in .NET Core 3.1 using NetOffice 2 alpha"
