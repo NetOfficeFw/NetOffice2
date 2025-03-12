bb2990fe253e79440b35e789d03ebd14041012d5
Jozef Izso <jozef.izso@gmail.com>
2025-03-12 16:52:30 +0100

    Define `IRibbonExtensibility` interface

---
+++ src/NetOffice/Office/IRibbonExtensibility.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using NetOffice.Runtime;
+
+namespace NetOffice.Office
+{
+    /// <summary>
+    /// The interface through which the Ribbon user interface (UI) communicates with a COM add-in to customize the UI.
+    /// </summary>
+    [ComImport]
+    [Guid(IID.IID_IRibbonExtensibilityGuid)]
+    [TypeLibType(TypeLibTypeFlags.FDispatchable)]
+    public interface IRibbonExtensibility
+    {
+        /// <summary>
+        /// Loads the XML markup, either from an XML customization file or from XML markup embedded in the procedure, that customizes the Ribbon user interface.
+        /// </summary>
+        /// <param name="ribbonId">The identifier of the ribbon control.</param>
+        [DispId(1)]
+        string? GetCustomUI(string? ribbonId);
+    }
+}
