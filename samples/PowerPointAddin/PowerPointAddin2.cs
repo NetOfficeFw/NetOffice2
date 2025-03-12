8a0c6898464ea6ccae206e0029954edc15191ba8
Jozef Izso <jozef.izso@gmail.com>
2025-03-12 18:20:48 +0100

    Custom addin prototype

---
+++ samples/PowerPointAddin/PowerPointAddin2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using NetOffice.Office;
+using NetOffice.Office.Core;
+using NetOffice.PowerPoint;
+
+namespace NetOfficeSamples
+{
+    [ComVisible(true)]
+    [Guid("CD92E3AE-72B2-4E9F-A148-54D5F2B00238")]
+    [ProgId("NetOfficeSamples.PowerPointAddin2")]
+    public class PowerPointAddin2 : NetOffice.PowerPoint.PowerPointAddin
+    {
+        protected override void OnConnection(Application application, ext_ConnectMode connectMode, COMAddIn addin, ref Array custom)
+        {
+            var description = addin.Description;
+            var name = application.Name;
+        }
+    }
+}
---
+++ src/NetOffice/PowerPoint/PowerPointAddin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using NetOffice.Office;
+using NetOffice.Office.Core;
+
+namespace NetOffice.PowerPoint
+{
+    public class PowerPointAddin : IDTExtensibility2
+    {
+        public void OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+        {
+            try
+            {
+                var ppApp = new Application(application);
+                var addin = new COMAddInClass(addInInst);
+
+
+                this.OnConnection(ppApp, connectMode, addin, ref custom);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Addin failed. {ex}");
+            }
+        }
+
+        public void OnDisconnection([In] ext_DisconnectMode removeMode, [In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAddInsUpdate([In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStartupComplete([In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeginShutdown([In, MarshalAs(29, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void OnConnection(Application application, ext_ConnectMode connectMode, COMAddIn addin, ref Array custom)
+        {
+        }
+    }
+}
